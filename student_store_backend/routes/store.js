e9d3f6c5e4d410a7dc55924c781db41aff948b3c
milesba4 <milesbaffour@gmail.com>
2022-07-05 14:40:40 -0400

    creating get request at "/" endpoint

--- student_store_backend/routes/store.js
+++ student_store_backend/routes/store.js
@@ -1,3 +1,17 @@
 const express = require("express")
 const router = express.Router()
 const Store = require("../models/store")
+
+
+router.get("/", async(req,res,next)=>{
+try{
+
+
+}catch(err){
+// calls list products and returns the results as the products key of an object
+ next(err)   
+}
+
+
+
+})
