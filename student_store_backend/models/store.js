5e85c1a08021aa4120972b2434abc1f1ed35c015
milesba4 <milesbaffour@gmail.com>
2022-07-05 14:31:43 -0400

    Creating listProducts method

--- student_store_backend/models/store.js
+++ student_store_backend/models/store.js
@@ -2,6 +2,27 @@ const db = require("../db")
 
 class Store {
 
+static async listProducts(){
+//run a SQL query that searches the database for all products and returns a list of them.
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
