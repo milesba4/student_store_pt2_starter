864899d8d61ce0b38890dfe2d1637a99ad1b80f6
milesba4 <milesbaffour@gmail.com>
2022-07-05 14:58:51 -0400

    Creating Order class and methods and exporting it

---
+++ student_store_backend/models/order.js
@@ -0,0 +1,22 @@
+const db = require("../db")
+
+class Order {
+
+static async listOrdersForUser(){
+//run a SQL query that searches the database for all order and returns a list of them.
+
+
+}
+
+
+static async createOrder(){
+    //run a SQL query that creates orders
+    
+    
+}
+    
+
+
+}
+
+module.exports = Order
